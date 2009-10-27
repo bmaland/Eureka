92bc341c6b4a99e2403fcd2d9fb318cd31f12677
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-10-27 10:46:56 +0100

    Cleaned up Java imports

--- lib/eureka.rb
+++ lib/eureka.rb
@@ -7,14 +7,6 @@ require "java"
 # weka.jar must be on your CLASSPATH
 require "weka.jar"
 
-# TODO to sep. file
-import "weka.classifiers.meta.FilteredClassifier"
-import "weka.classifiers.trees.J48"
-
-import "weka.classifiers.lazy.IBk"
-
-import "weka.filters.unsupervised.attribute.Remove"
-
 $:.unshift(here = File.dirname(__FILE__))
 
 require "eureka/core"
--- spec/stock_spec.rb
+++ spec/stock_spec.rb
@@ -1,5 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+import "weka.classifiers.meta.FilteredClassifier"
+import "weka.classifiers.trees.J48"
+import "weka.filters.unsupervised.attribute.Remove"
+
 describe "Stock data example" do
 
   before(:all) do
