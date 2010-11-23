93299276c3561594d1a21ee8d7abe92ed5129cd4
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2010-11-23 11:11:21 +0100

    Use provided jars instead of CLASSPATH. Also added Eureka::Version

--- lib/eureka.rb
+++ lib/eureka.rb
@@ -1,8 +1,18 @@
 require "java"
 
-# weka.jar must be on your CLASSPATH
-require "weka.jar"
-
 $:.unshift(here = File.dirname(__FILE__))
+Dir["#{here}/jar/*.jar"].each { |jar| require jar }
+Dir["#{here}/eureka/*.rb"].each { |rb| require rb }
 
-require "eureka/core"
+module Eureka
+
+  module Version
+    MAJOR = '0'
+    MINOR = '1'
+    REVISION = '0'
+    def self.combined
+      [MAJOR, MINOR, REVISION].join('.')
+    end
+  end
+
+end
